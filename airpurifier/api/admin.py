e06467d29f4d552c106b170cafe88f994c31a7db
bigpie1367 <bigpie1367@naver.com>
2019-08-12 12:16:56 +0900

    AirPurifier Server

--- airpurifier/api/admin.py
+++ airpurifier/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fine_Dust
+
+admin.site.register(Fine_Dust)
 
 # Register your models here.
--- airpurifier/api/models.py
+++ airpurifier/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Fine_Dust(models.Model) :
+    density = models.FloatField()
+    created_date = models.DateTimeField(default = timezone.now())
+
+    def __str__(self) :
+        return str(self.density)
